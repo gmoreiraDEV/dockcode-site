aa3a80f3f1f3b5116d71692b538645739140e2da
gmoreiraDEV <guilhermemoreira.dev@gmail.com>
2021-12-03 09:00:50 -0300

    add a meta tags in html

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,14 @@
-import { Stack, VStack } from "@chakra-ui/layout";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 class DocumentDockCode extends Document {
   render() {
     return (
       <Html>
         <Head>
+          <meta charSet='UTF-8' />
+          <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+          <meta
+            name='viewport'
+            content='width=device-width, initial-scale=1.0'></meta>
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link
             rel='preconnect'
